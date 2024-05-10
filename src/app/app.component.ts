a3e01f095427e4439051927fa5925ab863f7ca11
kjeshang <kunal@zypgallery.ca>
2024-05-09 21:17:16 -0700

    Newly generated project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-// import { AppModule } from './app.module';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,15 +7,10 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kunal-maven-analytics-data-projects';
-
-  
-
-
-  // List of Maven Analytics Data Projects ----------------------------------
 
   items: any[] = [
     {
@@ -33,7 +25,4 @@ export class AppComponent {
       date_added:"2020-09-09"
     }
   ];
-
-  // items: string[] = ['Item 1', 'Item 2', 'Item 3'];
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,9 @@
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
+// import { AppRoutingModule } from './app-routing.module';
 
 imports: [
   BrowserModule,
   NgbModule,
-  AppRoutingModule,
-  CommonModule
+//   AppRoutingModule,
 ]
-
-// @NgModule({
-//   declarations: [
-//     // Your components, directives, pipes
-//   ],
-//   imports: [
-//     BrowserModule,
-//     NgbModule,
-//     AppRoutingModule,
-//     CommonModule
-//   ],
-//   providers: [
-//     // Your services
-//   ],
-//   bootstrap: [
-//     // Your root component
-//   ]
-// })
-// export class AppModule { }
