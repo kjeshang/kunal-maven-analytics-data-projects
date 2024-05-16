df4e18b3e5cfa5bc5710cb0fef3c21c47a8914e3
kjeshang <kunal@zypgallery.ca>
2024-05-15 20:36:01 -0700

    Added Movie Ratings project. Also created navigation bar with search bar that can filter by project name & description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -9,8 +9,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'kunal-maven-analytics-data-projects';
+export class AppComponent implements OnInit {
+  // Variables -----------------------------------------------
+  title = "Kunal Maven Analytics Data Projects";
+  
+  searchTerm: string = '';
+
+  // List Variables ------------------------------------------------------------
 
   items: any[] = [
     {
@@ -23,6 +28,39 @@ export class AppComponent {
       num_records:5000,
       num_fields:14,
       date_added:"2020-09-09"
+    },
+    {
+      name:"Movie Ratings",
+      description:"Ratings and reviews for 15,000+ movies reviewed by Rotten Tomatoes.",
+      logo:"https://github.com/kjeshang/KunalMavenAnalyticsDataPlayground/blob/main/Movie_Ratings/Logo.png?raw=true",
+      project_link:"https://github.com/kjeshang/KunalMavenAnalyticsDataPlayground/tree/main/Movie_Ratings",
+      tags:["Entertainment"],
+      data_structure:"Single table",
+      num_records:16638,
+      num_fields:17,
+      date_added:"2020-09-09"
     }
   ];
+
+  filteredItems: any[] = [];
+  
+  // Functions -----------------------------------------------------------------
+  
+  ngOnInit(): void {
+    this.filteredItems = this.items;
+  }
+
+  onSearch(event: any): void {
+    this.searchTerm = event.target.value;
+    this.filterEntries();
+  }
+
+  filterEntries(): void {
+    const lowerCaseTerm = this.searchTerm.toLowerCase();
+    this.filteredItems = this.searchTerm ?
+      this.items.filter(item =>
+        item.name.toLowerCase().includes(lowerCaseTerm) ||
+        item.description.toLowerCase().includes(lowerCaseTerm)
+      ) : this.items;
+  }
 }
